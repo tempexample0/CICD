f802166f48c3c1bf73c01cac37fd23536f27e656
ghfw <55986927+ghfw@users.noreply.github.com>
2020-01-14 15:47:07 -0500

    Update addition.js
    obvious error

--- service/addition.js
+++ service/addition.js
@@ -3,8 +3,7 @@ var request = require('request');
 var addition = {
 
     find: function(req, res, next) {
-        var result = parseInt(req.params.number1) + parseInt(req.params.number2);
-        res.send('Addition Results: '+req.params.number1+' (+) '+req.params.number2+' = '+result);
+        var result = parseInt(req.params.params.number2+' = '+result);
    }
 };
 
