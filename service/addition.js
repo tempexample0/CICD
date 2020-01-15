ba19656a1f7f62da8275472e80b8f067ea28a615
ghfw <55986927+ghfw@users.noreply.github.com>
2020-01-15 16:24:15 -0500

    test testing

--- service/addition.js
+++ service/addition.js
@@ -1,11 +1,29 @@
 var request = require('request');
 
+var addHelp = {
+    add: function(n1, n2){
+        return parseInt(n1) + parseInt(n2);
+    }
+}
+
 var addition = {
 
     find: function(req, res, next) {
         var result = parseInt(req.params.number1) + parseInt(req.params.number2);
+        //var result = add(req.params.number1, req.params.number2);
         res.send('Addition Results: '+req.params.number1+' (+) '+req.params.number2+' = '+result);
-   }
+    },
+
+    add: function(n1, n2){
+        return parseInt(n1) + parseInt(n2);
+    }
 };
 
+function add(n1, n2){
+    return parseInt(n1) + parseInt(n2);
+}
+
+//module.exports = addHelp;
 module.exports = addition;
+
+
---
+++ test/test.js
@@ -0,0 +1,13 @@
+//import { result } from '../service/addition'
+//import {add} from './../service/addition.js';
+//var addHelp = require('./../service/addHelp')
+var addition = require('./../service/addition');
+
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function(){
+      assert.equal(5, addition.add(2, 3));
+    });
+  });
+});
